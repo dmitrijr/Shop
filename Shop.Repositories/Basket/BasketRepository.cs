8a9acd4f0a79c4dbc0b95f968669271fde37d5a9
juozas <muleviciusj@gmail.com>
2026-04-22 21:38:50 +0300

    #18 temos uždaviniai 1-5

--- Shop.Repositories/Basket/BasketRepository.cs
+++ Shop.Repositories/Basket/BasketRepository.cs
@@ -8,37 +8,68 @@ namespace Shop.Repositories
 
         public int Add(int userId, int productId, int count)
         {
-            var maxId = Baskets.Keys.Any() ? Baskets.Keys.Max() : 0;
-            var newId = maxId + 1;
-            var basket = new Basket()
-            {
-                UserId = userId,
-                Products = new()
-                {
-                    new ProductInBasket()
-                    {
-                        BasketId = newId,
-                        ProductId = productId,
-                        Count = count
-                    }
-                }
-            };
-
-            basket.Id = newId;
-
-            Baskets.Add(userId, basket);
-
+            var basket = GetInstance(userId);
+            AddBasketItem(basket, productId, count);
             return basket.Id;
         }
 
         public void Remove(int userId, int productId, int count)
         {
-            throw new NotImplementedException();
+            var basket = GetInstance(userId);
+            RemoveBasketItem(basket, productId, count);
         }
 
         public void RemoveAll(int userId, int productId)
         {
-            throw new NotImplementedException();
+            var basket = GetInstance(userId);
+            var product = basket.Products.Find(o => o.ProductId == productId);
+            basket.Products.Remove(product);
+        }
+
+        public Basket Get(int userId)
+        {
+            return Baskets[userId];
+        }
+
+        private Basket GetInstance(int userId)
+        {
+            Baskets.TryGetValue(userId, out Basket basket);
+            if (basket != null) return basket;
+
+            var maxId = Baskets.Keys.Any() ? Baskets.Keys.Max() : 0;
+            var newId = maxId + 1;
+            var newBasket = new Basket()
+            {
+                UserId = userId,
+                Id = newId,
+                Products = new List<ProductInBasket> { }
+            };
+            Baskets.Add(userId, newBasket);
+            return newBasket;
+        }
+
+        private void AddBasketItem(Basket basket, int productId, int count)
+        {
+            var product = basket.Products.Find(o => o.ProductId == productId);
+            if (product != null) product.Count += count;
+
+            else
+            {
+                var newProduct = new ProductInBasket
+                {
+                    BasketId = basket.Id,
+                    ProductId = productId,
+                    Count = count
+                };
+                basket.Products.Add(newProduct);
+            }
+        }
+
+        private void RemoveBasketItem(Basket basket, int productId, int count)
+        {
+            var product = basket.Products.Find(o => o.ProductId == productId);
+            if (product.Count > count) product.Count -= count;
+            else basket.Products.Remove(product);
         }
     }
 }
--- Shop.Repositories/Basket/IBasketRepository.cs
+++ Shop.Repositories/Basket/IBasketRepository.cs
@@ -1,3 +1,5 @@
+using Shop.Entities;
+
 namespace Shop.Repositories
 {
     public interface IBasketRepository
@@ -5,5 +7,6 @@ namespace Shop.Repositories
         int Add(int userId, int productId, int count);
         void Remove(int userId, int productId, int count);
         void RemoveAll(int userId, int productId);
+        Basket Get(int userId);
     }
 }
