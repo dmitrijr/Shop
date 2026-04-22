cd7bf8f6dae36f44e143b07f3aada9a0101c67a4
Dmitrij <dmitrij.ryb@gmail.com>
2026-04-22 20:14:33 +0300

    Add new column and migration scripts

---
+++ Shop.Data/Migrations/20260422170732_AddColumnToProduct.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CountInStock",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountInStock",
+                table: "Products");
+        }
+    }
+}
--- Shop.Entities/Product.cs
+++ Shop.Entities/Product.cs
@@ -5,5 +5,6 @@ namespace Shop.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public int CountInStock { get; set; }
     }
 }
